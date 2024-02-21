703a87f79634025f80e853d6f5139bd5526bb1c6
bruceouyang <bruceouyang@tencent.com>
2024-02-21 19:16:50 +0800

    feat: add coverage

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
-      formats: ["es", "cjs"],
-      fileName: "index",
-    },
+      entry: 'src/index.ts',
+      formats: ['es', 'cjs'],
+      fileName: 'index'
+    }
   },
   plugins: [
     dts({
-      exclude: ["**/__tests__", "**/*.test.ts"],
-      rollupTypes: true,
-    }),
-  ],
-});
+      exclude: ['**/__tests__', '**/*.test.ts'],
+      rollupTypes: true
+      // tsconfig: 'build.tsconfig.json'
+    })
+  ]
+})

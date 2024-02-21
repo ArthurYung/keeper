2873df73787c5defaea263d9900690563c939740
bruceouyang <bruceouyang@tencent.com>
2024-02-21 21:37:34 +0800

    chore: add coverage excludes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: 'index'
-    }
+      entry: "src/index.ts",
+      formats: ["es", "cjs"],
+      fileName: "index",
+    },
   },
   plugins: [
     dts({
-      exclude: ['**/__tests__', '**/*.test.ts'],
-      rollupTypes: true
+      exclude: ["**/__tests__", "**/*.test.ts"],
+      rollupTypes: true,
       // tsconfig: 'build.tsconfig.json'
-    })
-  ]
-})
+    }),
+  ],
+  test: {
+    coverage: {
+      exclude: ["src/type.ts", "src/index.ts"],
+    },
+  },
+});
